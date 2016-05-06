020ffeba7396e05d4152eeaccefff294c5e63eca
Collin Adams <collinadams04@gmail.com>
2016-05-06 12:56:46 -0700

    Add spinner for loading force layout

--- app.js
+++ app.js
@@ -1,203 +1,116 @@
-var width = 1000;
-var height = 750;
+var spinner;
 
-// var nodes = [
-//   {
-//     x: width/3, 
-//     y: height/2
-//   },
-//   {
-//     x: 2*width/3, 
-//     y: height/2}
-// ];
-
-// var links = [
-//   {
-//     source: 0, 
-//     target: 1
-//   }
-// ];
+var spinnerOptions = {
+  lines: 13,
+  length: 28,
+  width: 14,
+  radius: 42,
+  scale: 1,
+  corners: 1,
+  color: '#000',
+  opacity: 0.25,
+  rotate: 0,
+  direction: 1,
+  speed: 1,
+  trail: 60,
+  fps: 20,
+  zIndex: 2e9,
+  className: 'spinner',
+  top: '50%',
+  left: '50%',
+  shadow: false,
+  hwaccel: false ,
+  position: 'absolute',
+};
 
 var rawTestDNASequenceString = 'TTGGGGGGACTGGGGCTCCCATTCGTTGCCTTTATAAATCCTTGCAAGCCAATTAACAGGTTGGTGAGGGGCTTGGGTGAAAAGGTGCTTAAGACTCCGT';
 
 var rawTestDBN = '...(((((.(...).)))))........(((((.....((..(.((((((..(((.((...)).)))..)))))).).)))))))...............';
 
+var drawDNA = function(drawData){
+  var width = 600;
+  var height = 300;
+  var alreadyPositioned = !!(drawData.nodes && drawData.nodes[0].fixed);
 
-var dnaSequenceStringToArray = function(dnaSequenceString){
-  var dnaSequenceArray = dnaSequenceString
-                          .split('')
-                          .map(function(nucleotideLetter){
-                            var nucleotideObject = {nucleotideLetter: nucleotideLetter};
-                            if(nucleotideLetter === 'A'){
-                              nucleotideObject.color = 'green';
-                            }else if(nucleotideLetter === 'C'){
-                              nucleotideObject.color = 'blue';
-                            }else if(nucleotideLetter === 'G'){
-                              nucleotideObject.color = 'black';
-                            }else if(nucleotideLetter === 'T'){
-                              nucleotideObject.color = 'red';
-                            }else if(nucleotideLetter === 'N'){
-                              nucleotideObject.color = 'yellow';
-                            }
-                            return nucleotideObject;
-                          });
-
-  return dnaSequenceArray;
-};
-
-//the below function assumes that the dbnString input has balanced parens
-//TODO: optimize quadratic time complexity
-var findIndexOfMatchingBase = function(DBNSubArray){
-  var parenCounter = 1;
-  for(var i = 1; i < DBNSubArray.length; i++){
-    var currCharacter = DBNSubArray[i];
-    if(currCharacter === '.'){
-      continue;
-    }else if(currCharacter === '('){
-      parenCounter++;
-    }else if(currCharacter === ')'){
-      parenCounter--;
-    }else{
-      return 'The DBN String inputh contains an invalid character';
-    }
-    if(parenCounter === 0){
-      return i;
-    }
+  if(!alreadyPositioned){
+    var target = document.getElementById('spinner');
+    spinner = new Spinner(spinnerOptions).spin(target);
   }
-};
 
-var findPairedBasesInDBA = function(DBNString){
-  var arrayOfPairedBases = [];
-  var DBNArray = DBNString.split('');
-  for(var j = 0; j < DBNArray.length; j++){
-    var currCharacter = DBNArray[j];
-    if(currCharacter === '('){
-      var basePairSubArray = DBNArray.slice(j);
-      arrayOfPairedBases.push({
-        source: j,
-        target: j + findIndexOfMatchingBase(basePairSubArray)
+  var svg = d3.select('body').append('svg')
+              .attr('width', width)
+              .attr('height', height);
+
+  var force = d3.layout.force()
+              .size([width, height])
+              .nodes(drawData.nodes)
+              .links(drawData.links)
+              .linkDistance(8)
+              .charge(-30);
+
+  var link = svg.selectAll('.link')
+              .data(drawData.links)
+              .enter().append('line')
+              .attr('class', 'link')
+              .style('visibility', 'hidden');
+
+  var node = svg.selectAll('circle')
+              .data(drawData.nodes)
+              .enter().append('circle')
+              .attr('r', 4)
+              .attr('fill', function(d){
+                return d.color;
+              })
+              .style('visibility', 'hidden');
+
+  var updateSvgPositions = function(){
+    node.attr('cx', function(d, i){
+      return d.x;
+    })
+    .attr('cy', function(d, i){
+      return d.y;
+    })
+    .style('visibility', 'visible');
+
+    link.attr('x1', function(d){
+      return d.source.x;
+    })
+    .attr('y1', function(d){
+      return d.source.y;
+    })
+    .attr('x2', function(d){
+      return d.target.x;
+    })
+    .attr('y2', function(d){
+      return d.target.y;
+    })
+    .style('visibility', 'visible');
+  };     
+
+  if(!alreadyPositioned){
+    force.on('end', function(){
+      updateSvgPositions();
+      drawData.nodes.forEach(function(d){
+        d.fixed = true;
+        console.log(d);
       });
-    }else{
-      continue;
-    }
-  }
-  return arrayOfPairedBases;
-};
-
-var makeLinksForPhosphateBackbone = function(DBNString){
-  var arrayOfPhosphateBackboneLinks = [];
-  var DBNArray = DBNString.split('');
-  for(var i = 0; i < DBNArray.length - 1; i++){
-    arrayOfPhosphateBackboneLinks.push({
-      source: i,
-      target: i + 1
+      drawDNA(drawData);
     });
-  }
-  return arrayOfPhosphateBackboneLinks;
+    force.start();
+  }else{
+    updateSvgPositions();
+    spinner.stop();
+  }        
+
+  // force.on('tick', function(){
+
+  // })
 };
 
-var nodes = dnaSequenceStringToArray(rawTestDNASequenceString);
-var links = findPairedBasesInDBA(rawTestDBN).concat(makeLinksForPhosphateBackbone(rawTestDBN));
+var sampleData = {
+  nodes: dnaSequenceStringToArray(rawTestDNASequenceString),
+  links: findPairedBasesInDBA(rawTestDBN).concat(makeLinksForPhosphateBackbone(rawTestDBN))
+};
 
-var svg = d3.select('body').append('svg')
-            .attr('width', width)
-            .attr('height', height);
-
-var force = d3.layout.force()
-            .size([width, height])
-            .nodes(nodes)
-            .links(links)
-            .linkDistance(10)
-            .charge(-100);
-
-// var link = svg.selectAll('.link')
-//             .data(links)
-//             .enter().append('line')
-//             .attr('class', 'link');
-
-// var node = svg.selectAll('.node')
-//             .data(nodes)
-//             .enter().append('circle')
-//             .attr('class', 'node');
-
-var link = svg.selectAll('line')
-            .data(links)
-            .enter().append('line')
-            .attr('class', 'link');
-
-var node = svg.selectAll('circle')
-            .data(nodes)
-            .enter().append('circle')
-            .attr('r', 5)
-            .attr('fill', function(d){
-              return d.color;
-            });
-
-
-
-
-
-
-force.on('tick', function(){
-  node.attr('cx', function(d, i){
-    return d.x;
-  })
-  .attr('cy', function(d, i){
-    return d.y;
-  });
-
-  link.attr('x1', function(d){
-    return d.source.x;
-  })
-  .attr('y1', function(d){
-    return d.source.y;
-  })
-  .attr('x2', function(d){
-    return d.target.x;
-  })
-  .attr('y2', function(d){
-    return d.target.y;
-  });
-})
-
-force.start();
-// force.on('end', function(){
-//   node.attr('r', width/25)
-//       .attr('cx', function(d){
-//         return d.x;
-//       })
-//       .attr('cy', function(d){
-//         return d.y;
-//       });
-
-//   link.attr('x1', function(d){
-//         return d.source.x;
-//       })
-//       .attr('y1', function(d){
-//         return d.source.y;
-//       })
-//       .attr('x2', function(d){
-//         return d.target.x;
-//       })
-//       .attr('y2', function(d){
-//         return d.target.y;
-//       });
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+drawDNA(sampleData);
 
