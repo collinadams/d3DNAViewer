df96ceef00c236e2c32a9ca836c46f606d5bc03f
Collin Adams <collinadams04@gmail.com>
2016-05-05 17:44:11 -0700

    Add links for phosphate backbone and paired bases

--- app.js
+++ app.js
@@ -43,10 +43,61 @@ var dnaSequenceStringToArray = function(dnaSequenceString){
                           });
 
   return dnaSequenceArray;
-}
+};
+
+//the below function assumes that the dbnString input has balanced parens
+//TODO: optimize quadratic time complexity
+var findIndexOfMatchingBase = function(DBNSubArray){
+  var parenCounter = 1;
+  for(var i = 1; i < DBNSubArray.length; i++){
+    var currCharacter = DBNSubArray[i];
+    if(currCharacter === '.'){
+      continue;
+    }else if(currCharacter === '('){
+      parenCounter++;
+    }else if(currCharacter === ')'){
+      parenCounter--;
+    }else{
+      return 'The DBN String inputh contains an invalid character';
+    }
+    if(parenCounter === 0){
+      return i;
+    }
+  }
+};
+
+var findPairedBasesInDBA = function(DBNString){
+  var arrayOfPairedBases = [];
+  var DBNArray = DBNString.split('');
+  for(var j = 0; j < DBNArray.length; j++){
+    var currCharacter = DBNArray[j];
+    if(currCharacter === '('){
+      var basePairSubArray = DBNArray.slice(j);
+      arrayOfPairedBases.push({
+        source: j,
+        target: j + findIndexOfMatchingBase(basePairSubArray)
+      });
+    }else{
+      continue;
+    }
+  }
+  return arrayOfPairedBases;
+};
+
+var makeLinksForPhosphateBackbone = function(DBNString){
+  var arrayOfPhosphateBackboneLinks = [];
+  var DBNArray = DBNString.split('');
+  for(var i = 0; i < DBNArray.length - 1; i++){
+    arrayOfPhosphateBackboneLinks.push({
+      source: i,
+      target: i + 1
+    });
+  }
+  return arrayOfPhosphateBackboneLinks;
+};
 
 var nodes = dnaSequenceStringToArray(rawTestDNASequenceString);
-var links = [{source: 0, target: 1}, {source: 2, target: 0}, {source: 1, target: 2}];
+var links = findPairedBasesInDBA(rawTestDBN).concat(makeLinksForPhosphateBackbone(rawTestDBN));
 
 var svg = d3.select('body').append('svg')
             .attr('width', width)
